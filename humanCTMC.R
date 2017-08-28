02229e6b2cfc1284ad3549a2c8bebf7cf25f5d7e
slwu89 <slwu89@berkeley.edu>
2017-08-27 21:39:23 -0700

    sinusoidal hazard function

--- humanCTMC.R
+++ humanCTMC.R
@@ -49,13 +49,54 @@ simpleHuman <- function(maxSteps=1e3){
   return(list(states=states,times=times,duration=duration))
 }
 
-out = simpleHuman(maxSteps = 1000)
+out = simpleHuman(maxSteps = 50)
 
+xlim = c(0,max(out$times)+1)
+ylim = c(0,5)
 
+plot(1,type="n",xaxt="n",yaxt="n",ylab="State Occupancy",xlab="Time (Days)",xlim=xlim,ylim=ylim)
+ttMax = max(out$times)/24
+axis(side = 1,at = (0:ttMax)*24,labels = 0:ttMax)
+cols = c("blue","grey50","purple","grey20")
+for(i in 1:length(out$states)){
+  if(i==length(out$states)){
+    switch(out$states[i],
+           H = {
+             segments(x0 = out$times[i],y0 = 1,x1 = out$times[i]+1,y1 = 1,col = cols[1],lwd = 3)
+           },
+           H2W = {
+             segments(x0 = out$times[i],y0 = 2,x1 = out$times[i]+1,y1 = 2,col = cols[2],lwd = 3)
+           },
+           W = {
+             segments(x0 = out$times[i],y0 = 3,x1 = out$times[i]+1,y1 = 3,col = cols[3],lwd = 3)
+           },
+           W2H = {
+             segments(x0 = out$times[i],y0 = 4,x1 = out$times[i]+1,y1 = 4,col = cols[4],lwd = 3)
+           }
+    )
+  } else {
+    switch(out$states[i],
+            H = {
+              segments(x0 = out$times[i],y0 = 1,x1 = out$times[i+1],y1 = 1,col = cols[1],lwd = 3)
+            },
+            H2W = {
+              segments(x0 = out$times[i],y0 = 2,x1 = out$times[i+1],y1 = 2,col = cols[2],lwd = 3)
+            },
+            W = {
+              segments(x0 = out$times[i],y0 = 3,x1 = out$times[i+1],y1 = 3,col = cols[3],lwd = 3)
+            },
+            W2H = {
+              segments(x0 = out$times[i],y0 = 4,x1 = out$times[i+1],y1 = 4,col = cols[4],lwd = 3)
+            }
+           )
+  }
+}
 
 t=seq(from=0,to=24*60,by=0.01)
 plot(sin((2*pi*t)/1440),type="l")
-plot({sin((2*pi*t)/1440) + cos((2*pi*t)/1440)},type = "l")
+plot({
+    sin((2*pi*t)/1440) + cos((2*pi*t)/1440)
+  },type = "l")
 
 hazFunc = function(t){
   sin((2*pi*t)/1440) + cos((2*pi*t)/1440)
@@ -63,3 +104,18 @@ hazFunc = function(t){
 
 out = sin((2*pi*t)/1440) + cos((2*pi*t)/1440)
 integrate(f = hazFunc,lower = 0,upper = 1440,subdivisions = 1e3)
+
+
+
+
+hazardFunction <- function(t,sinK,cosK,pow=1){
+  sin((sinK*pi*t)/1440) + cos((cosK*pi*t)/1440)
+}
+
+tseq = 0:(60*24)
+
+plotHazard <- function(sinK,cosK,pow=1){
+  plot(x = tseq,y = hazardFunction(tseq,sinK,cosK,pow),type="l",xlab="Time of Day",ylab="Hazard")
+}
+plotHazard(sinK = 2,cosK = 2)
+
